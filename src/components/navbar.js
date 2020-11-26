0e389decbf9795560e5d4efcc9c95d18dfe87f09
harishm72 <harishchowdary72@gmail.com>
2020-11-27 00:28:17 +0530

    Add navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 function NavBar() {
-  return <nav>NavBar</nav>;
+  return (
+    <nav className="nav-menu">
+      <Link href="/mens-wear">Men's Outerwear</Link>
+      <Link href="/womens-wear">Ladies Outerwear</Link>
+      <Link href="/mens-tshirt">Men's T-Shirts</Link>
+      <Link href="/womens-tshirt">Ladies T-Shirts</Link>
+    </nav>
+  );
 }
 
 export default NavBar;
