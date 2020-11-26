133855775ad8d3e1a32efba41309d36ff35e68e1
harishm72 <harishchowdary72@gmail.com>
2020-11-27 00:29:47 +0530

    add mens-wear component

---
+++ src/pages/mens-wear.js
@@ -0,0 +1,50 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+import NavBar from "../components/navbar";
+import ProductCard from "../components/productCard";
+import { useEffect, useState } from "react";
+
+function MensOuterwear() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const res = await (await fetch("../data/mens_outerwear.json")).json();
+        setProducts(res);
+      };
+
+      fetchData();
+    } catch (error) {}
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <NavBar />
+      <section className="mens-outerwear">
+        <img
+          className="mens-outerwear-img"
+          src="../images/mens_outerwear.jpg"
+          alt="Men's Outerwear"
+        />
+        <p className="shop-category">Men's Outerwear</p>
+        <p className="mens-outerwear-quantity">({products.length} items)</p>
+      </section>
+      <ul className="mens-apparel">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            imgSrc={`./${product.image}`}
+            title={product.title}
+            price={product.price}
+          />
+        ))}
+      </ul>
+      <Footer />
+    </div>
+  );
+}
+
+export default MensOuterwear;
